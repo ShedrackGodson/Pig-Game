3a50acb118b502829eef4e7be9702344d20e3014
ShedrackGodson <hedrickgodson23@gmail.com>
2021-01-17 03:14:06 +0300

    final commit

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ document.querySelector("#score--0").textContent = 0;
 document.querySelector("#score--1").textContent = 0;
 document.querySelector("#current--0").textContent = 0;
 document.querySelector("#current--1").textContent = 0;
+document.querySelector("#winner--0").style.display = "none";
+document.querySelector("#winner--1").style.display = "none";
 
 // rollDice Function
 function rollDice(){
@@ -35,19 +37,48 @@ function rollDice(){
     if(dice !== 1){
         roundScore += dice;
         document.querySelector("#current--" + activePlayer).textContent = roundScore;
-        scores[activePlayer] += roundScore;
-        document.querySelector("#score--" + activePlayer).textContent = scores[activePlayer];
+        // scores[activePlayer] += roundScore;
+        // document.querySelector("#score--" + activePlayer).textContent = scores[activePlayer];
     } else {
         // Next Player turn
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        roundScore = 0;
-
-        document.querySelector("#current--1").textContent = 0;
-        document.querySelector("#current--0").textContent = 0;
+        newFunction();
     }
 }
 
 // holdScore Function
 function holdScore(){
-    console.log("Hold scores..");
+    // 1. Add Current Score to the Global Score
+    scores[activePlayer] += roundScore;
+
+    // 2. Update the UI
+    document.querySelector("#score--" + activePlayer).textContent = scores[activePlayer];
+
+    // 3. Check if the player won the game
+    if(scores[activePlayer] >= 50){
+        // alert(activePlayer + 1 + " WON");
+        document.querySelector("#winner--"+activePlayer).style.display = "block";
+        document.querySelector(".btn--roll").disabled = true;
+        document.querySelector(".btn--hold").disabled = true;
+    }
+    // Next Player turn
+    newFunction();
+}
+
+function newFunction() {
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    roundScore = 0;
+
+    document.querySelector("#current--1").textContent = 0;
+    document.querySelector("#current--0").textContent = 0;
+
+    document.querySelector(".player--1").classList.toggle("player--active");
+    document.querySelector(".player--0").classList.toggle("player--active");
+
+    document.querySelector(".dice").style.display = "none";
+}
+
+// newGame Function
+function newGame(){
+    // Just reload the window to form a new game;
+    window.location.reload();
 }
